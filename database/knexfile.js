0bfdfde53d908ca69b5be8963bb0dc5206d154fb
Erwin France <erwinfrance@gmail.com>
2016-08-29 17:23:24 +0200

    Committing database url env var for heroku

--- database/knexfile.js
+++ database/knexfile.js
@@ -24,14 +24,7 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: {
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            charset: 'utf8',
-            ssl: true
-        },
+        connection: process.env.DATABASE_URL,
     //pool: {
     //  min: 2,
     //  max: 10
