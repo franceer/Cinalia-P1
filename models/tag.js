e73100449aaf9fe920434d81bb5f1b0f8c185943
Erwin France <erwinfrance@gmail.com>
2016-10-07 03:23:48 +0200

    Fixed search tag

--- models/tag.js
+++ models/tag.js
@@ -51,8 +51,8 @@ var Tag = bookshelf.Model.extend({
                         qb.joinRaw('LEFT JOIN user_bookmarks b1 ON b1.bookmark_id = products.id AND b1.user_id = ' + user.id + ' AND b1.bookmark_type = \'product\'');                    
 
                     qb.join('brands as b', 'b.id', '=', 'products.brand_id');
-                    qb.join('tags_products as cp', 'products.id', '=', 'cp.product_id');
-                    qb.join('tags as t', 'cp.tag_id', '=', 't.id');
+                    qb.join('products_tags as pt', 'products.id', '=', 'pt.product_id');
+                    qb.join('tags as t', 'pt.tag_id', '=', 't.id');
                     qb.where('t.path', '~', '*.' + tagPath + '.*');
                     qb.orderByRaw('(products.created_at, products.id) DESC');
                     qb.select(bookshelf.knex.raw('products.id, \'<span>\' || b.name || \'</span> \' || products.name  as name, products.description, products.picture_url, products.picture_alt, products.picture_title, \'products\' as section_url' + (user ? ', b1.id as bookmark_id' : '')));
@@ -63,8 +63,8 @@ var Tag = bookshelf.Model.extend({
                     if (user) 
                         qb.joinRaw('LEFT JOIN user_bookmarks b ON b.bookmark_id = locations.id AND b.user_id = ' + user.id + ' AND b.bookmark_type = \'location\'');                    
 
-                    qb.join('tags_locations as cloc', 'locations.id', '=', 'cloc.location_id');
-                    qb.join('tags as t', 'cloc.tag_id', '=', 't.id');
+                    qb.join('locations_tags as loct', 'locations.id', '=', 'loct.location_id');
+                    qb.join('tags as t', 'loct.tag_id', '=', 't.id');
                     qb.where('t.path', '~', '*.' + tagPath + '.*');
                     qb.orderBy('created_at', 'desc');
                     qb.select(bookshelf.knex.raw('locations.id, locations.name, locations.description, locations.picture_url, locations.picture_alt, locations.picture_title, \'locations\' as section_url' + (user ? ', b.id as bookmark_id' : '')));
@@ -75,8 +75,8 @@ var Tag = bookshelf.Model.extend({
                     if (user)
                         qb.joinRaw('LEFT JOIN user_bookmarks b ON b.bookmark_id = looks.id AND b.user_id = ' + user.id + ' AND b.bookmark_type = \'look\'');
 
-                    qb.join('tags_looks as l', 'looks.id', '=', 'l.look_id');
-                    qb.join('tags as t', 'l.tag_id', '=', 't.id');
+                    qb.join('looks_tags as lt', 'looks.id', '=', 'lt.look_id');
+                    qb.join('tags as t', 'lt.tag_id', '=', 't.id');
                     qb.where('t.path', '~', '*.' + tagPath + '.*');
                     qb.orderBy('created_at', 'desc');
                     qb.select(bookshelf.knex.raw('looks.id, looks.name, looks.description, NULL, NULL, NULL, \'looks\' as section_url' + (user ? ', b.id as bookmark_id' : '')));
@@ -87,8 +87,8 @@ var Tag = bookshelf.Model.extend({
                     if (user)
                         qb.joinRaw('LEFT JOIN user_bookmarks b ON b.bookmark_id = sets.id AND b.user_id = ' + user.id + ' AND b.bookmark_type = \'set\'');
 
-                    qb.join('tags_sets as s', 'sets.id', '=', 's.set_id');
-                    qb.join('tags as t', 's.tag_id', '=', 't.id');
+                    qb.join('sets_tags as st', 'sets.id', '=', 'st.set_id');
+                    qb.join('tags as t', 'st.tag_id', '=', 't.id');
                     qb.where('t.path', '~', '*.' + tagPath + '.*');
                     qb.orderBy('created_at', 'desc');
                     qb.select(bookshelf.knex.raw('sets.id, sets.name, sets.description, sets.picture_url, sets.picture_alt, sets.picture_title, \'sets\' as section_url' + (user ? ', b.id as bookmark_id' : '')));
